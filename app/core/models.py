b3555adb457d7cbcc52ff538357cc25cc48e404e
Guilherme Fantuci <gfantuci@gmail.com>
2022-09-10 17:24:53 -0400

    124. Modify recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 Database models
 """
 import uuid
+import os
 
 from django.conf import settings
 from django.db import models
@@ -10,6 +11,14 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    extension = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{extension}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -67,6 +76,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
