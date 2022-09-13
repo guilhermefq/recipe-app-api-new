397da80bbf73c4e9bb72d089e2bea6ed5e65aeed
Guilherme Fantuci <gfantuci@gmail.com>
2022-09-13 11:13:58 -0400

    143. Update Django settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+^zfhj+q-4^gmyog)i#(^6ro0nkf1hd*3^v1h#xnd5a2bnbzl^'
+SECRET_KEY = os.environ.get('SECRET', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
